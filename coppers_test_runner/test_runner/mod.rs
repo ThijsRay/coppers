beadce6d27d0f3c5d0e65f5bb5f9edaead4c70d7
Jeffrey Bouman <jeffrey.bouman@hotmail.com>
2022-03-29 14:05:50 +0200

    Use correct name for output file

--- coppers_test_runner/test_runner/mod.rs
+++ coppers_test_runner/test_runner/mod.rs
@@ -100,9 +100,9 @@ fn write_to_json(
 
     // Convert test results in JSON object
     let json_tests = serde_json::to_string(&passed_tests).unwrap();
-    let full_json = format!("{{\"timestamp\":{timestamp},\"head\":{head_hash_json}\"total_time\":{total_us},\"total_consumption\":{total_uj},\"overhead_time\":{overhead_us},\"overhead_consumption\":{overhead_uj},\"tests\":{json_tests}}}");
+    let full_json = format!("{{\"timestamp\":{timestamp},\"head\":{head_hash_json}\"total_time\":{total_us},\"total_consumption\":{total_uj},\"overhead_time\":{overhead_us},\"overhead_consumption\":{overhead_uj},\"number_of_repeats\":{REPEAT_TESTS_AMOUNT_OF_TIMES},\"tests\":{json_tests}}}");
 
-    let json_file_name = format!("target/copper_results-{timestamp}.json");
+    let json_file_name = format!("target/coppers_results-{timestamp}.json");
     let mut file = File::create(json_file_name).unwrap();
     file.write_all(full_json.as_bytes())
         .expect("Writing of JSON file failed.");
