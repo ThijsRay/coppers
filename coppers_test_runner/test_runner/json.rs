89d23bf4dec546bb7be23c8e52a1fd1b33a993fe
Thijs Raymakers <thijs@raymakers.nl>
2022-03-30 14:33:52 +0200

    Change field names and output directory of json reports

--- coppers_test_runner/test_runner/json.rs
+++ coppers_test_runner/test_runner/json.rs
@@ -7,27 +7,32 @@ use std::time::SystemTime;
 use super::CompletedTest;
 use super::REPEAT_TESTS_AMOUNT_OF_TIMES;
 
+#[derive(serde::Serialize)]
 struct JsonResult {
     timestamp: u64,
-    head: Vec<u8>,
+    head: String,
+    total_us: u128,
+    total_uj: u128,
+    overhead_us: u128,
+    overhead_uj: u128,
+    number_of_repeats: usize,
     tests: Vec<CompletedTest>,
 }
 
 pub(crate) fn write_to_json(
-    passed_tests: Vec<CompletedTest>,
+    tests: Vec<CompletedTest>,
     total_us: u128,
     total_uj: u128,
     overhead_us: u128,
     overhead_uj: u128,
 ) {
     // Get git hash of last commit
-    // WARNING: ASSUMING REPO EXISTS!
     let current_directory = current_dir().unwrap();
     let current_path = current_directory.as_path().to_str().unwrap();
     let repo = Repository::open(current_path).unwrap();
     let head_hash = repo.head().unwrap().target().unwrap();
     let head_hash_bytes = head_hash.as_bytes();
-    let head_hash_json = serde_json::to_string(&head_hash_bytes).unwrap();
+    let head = hex::encode(head_hash_bytes);
 
     // Get the timestamp of the current time
     let timestamp = SystemTime::now()
@@ -35,12 +40,21 @@ pub(crate) fn write_to_json(
         .unwrap()
         .as_secs();
 
-    // Convert test results in JSON object
-    let json_tests = serde_json::to_string(&passed_tests).unwrap();
-    let full_json = format!("{{\"timestamp\":{timestamp},\"head\":{head_hash_json},\"total_time\":{total_us},\"total_consumption\":{total_uj},\"overhead_time\":{overhead_us},\"overhead_consumption\":{overhead_uj},\"number_of_repeats\":{REPEAT_TESTS_AMOUNT_OF_TIMES},\"tests\":{json_tests}}}");
+    let output = JsonResult {
+        timestamp,
+        head,
+        total_us,
+        total_uj,
+        overhead_us,
+        overhead_uj,
+        number_of_repeats: REPEAT_TESTS_AMOUNT_OF_TIMES,
+        tests,
+    };
 
-    let json_file_name = format!("target/coppers_results-{timestamp}.json");
+    // Convert test results in JSON object
+    let output_json = serde_json::to_string(&output).unwrap();
+
+    let json_file_name = format!("target/coppers_results/coppers_results-{timestamp}.json");
     let mut file = File::create(json_file_name).unwrap();
-    file.write_all(full_json.as_bytes())
-        .expect("Writing of JSON file failed.");
+    file.write_all(output_json.as_bytes()).unwrap()
 }
