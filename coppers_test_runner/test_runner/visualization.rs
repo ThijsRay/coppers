e74a4c03eb67c7e87d7948e0b6b919ee09637c0d
Thijs Raymakers <thijs@raymakers.nl>
2022-03-30 16:45:09 +0200

    Add https in front of datatable include

--- coppers_test_runner/test_runner/visualization.rs
+++ coppers_test_runner/test_runner/visualization.rs
@@ -26,14 +26,14 @@ TEMPLATE = """
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link href="//cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+        <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
         <style>body{ margin:0 100; background:whitesmoke; }</style>
         <meta charset="UTF-8"/>
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="//cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
         <h1 class="display-3 align-center"><img src="../../logo/coppers-logo.svg" alt="Coppers logo" class="img-thumbnail" width="200"/>Coppers test energy usage</h1>
 
         <h2>Most energy consuming tests</h2>
